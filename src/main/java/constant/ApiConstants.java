5c16c86c81844f9e1535868d5197f925f1ece814
anurag pandey <anuragpandey930565@gmail.com>
2026-07-25 18:23:42 +0530

    adding apiconstant in framework

---
+++ src/main/java/constant/ApiConstants.java
@@ -0,0 +1,12 @@
+package constant;
+
+public final class ApiConstants {
+
+    private ApiConstants() {
+        // Prevent object creation
+    }
+
+    public static final String AUTH = "/auth";
+    public static final String BOOKING = "/booking";
+    public static final String PING = "/ping";
+}
